4df900e4c38fc99442aef2fd9da8993fc94646b7
mugilanmindshare <108856140+mugilanmindshare@users.noreply.github.com>
2022-07-29 11:59:18 +0530

    Add files via upload

---
+++ powerbi.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+components.html("""<iframe title="twitter_analysis" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=d0558e13-4b99-48c5-a79e-cca66e8d5966&autoAuth=true&ctid=322ea77d-46ae-4b34-abac-c7897eeb87af&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>""",height=600,)
